091ee714c17100f4004dfdd95878f9ef2bda266a
nikilgs <nikilgs143@gmail.com>
2025-09-08 13:11:13 +0530

    your message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import TaskManager from "./components/TaskManager";
 import Login from "./components/Login";
-import Test from "./component1/Test"
+import Test from "./components1/Test"
 function App() {
   const [user, setUser] = useState(null);
 
